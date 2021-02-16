dcb3d3220b1a0a05cfeadf84a372495e24a29292
Tristanoshier <tsoshier@bsu.edu>
2021-02-15 21:43:34 -0500

    for loop example

---
+++ chThree/reverse-string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char s[]);
+
+int main() 
+{
+    char name[] = "tristan";
+    
+    printf("%s\n", name);
+    reverse(name);
+    printf("%s\n", name);
+}
+
+void reverse(char s[])
+{
+    int c, i, j;
+
+    for(i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+}
+
